473a9a477d251aaacfec52f381e4d5b3e2944c43
EmilyK <koskinen.emily@gmail.com>
2019-11-19 11:50:48 +0200

    card component added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Highscore from './components/Highscore';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
       <Highscore/>
+   <Card/>
     </div>
   );
 }
