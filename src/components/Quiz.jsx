71d745e5df563ceb4a30ae58bb38c5d81e84fcd3
Lasse Valtanen <lasse.valtanen@gmail.com>
2019-11-19 16:42:45 +0200

    Lisätty toimiva quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,143 +1,156 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { fetchAllData } from '../service';
 
 export class Quiz extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = { current: 0, dataSet: [], correct: 0, lives: 3 }
-        // this.handleClick = this.handleClick.bind(this)
-
-    } // end constructor
+        this.handleClick = this.handleClick.bind(this)
+    } 
 
     componentDidMount() {
-        fetch('/api/questions')
-        .then(res => {return res.json()})
-        .then(data => this.setState( { dataSet: data }))
-        .then(console.log(this.state.dataSet))
+        this.fetchDataList();
     }
 
+    fetchDataList = () => {
+        fetchAllData().then(allData => {
+            this.setState({ dataSet: allData });
+            console.log(this.state)
+        })
+    }
 
-//     handleClick(choice) {
-//         if (choice === this.state.dataSet[this.state.current].correct) {
-//             this.setState({ correct: this.state.correct + 1 })
-//         } else {
-//             this.setState({ lives: this.state.lives - 1 })
-//         }
+    handleClick(choice) {
+        console.log(choice)
+        if (choice === this.state.dataSet[this.state.current].correct_answer) {
+            this.setState({ correct: this.state.correct + 1 })
+        } else {
+            if ( this.state.lives > 1) {
+            this.setState({ lives: this.state.lives - 1 })
+            } else {
+                window.alert("Kuolit. 18 000 €");
+            }
+        }
 
-//         if (this.state.current === 9) {
-//             this.setState({ current: 0 })
-//             this.setState({ lives: 0 })
-//             this.setState({ correct: 0 })
-//         } else {
-//             this.setState({ current: this.state.current + 1 })
-//         }
-//     }
+        if (this.state.current === 9) {
+            this.setState({ current: 0 })
+            this.setState({ lives: 0 })
+            this.setState({ correct: 0 })
+        } else {
+            this.setState({ current: this.state.current + 1 })
+        }
+    }
 
     render() {
         return (
-            
+
             <div>
-                moi
-                {/* <ScoreArea correct={this.state.correct} lives={this.state.lives} />
-                <QuizArea handleClick={this.handleClick} dataSet={this.state.dataSet[this.state.current]} /> */}
+                <ScoreArea correct={this.state.correct} lives={this.state.lives} />
+                <QuizArea handleClick={this.handleClick} dataSet={this.state.dataSet[this.state.current]} />
             </div>
         )
     }
 }
+function Question(props) {
+    var style = {
+        color: "red",
+    }
+    if (!props.dataSet) {
+        return <p>Loading...</p>
+    }
+    return (
+        <h1 style={style}>{props.dataSet.question}</h1>
+    )
+}
 
-// function Question(props) {
-//     var style = {
-//         color: "red",
-//     }
-//     return (
-//         <h1 style={style}>{props.dataSet.question}</h1>
-//     )
-// }
+function Answer(props) {
+    var style = {
+        width: "100%",
+        height: 50,
+        color: "black"
+    }
+    return (
+        <div>
+            <button style={style} onClick={() => props.handleClick(props.choice)}>{props.answer}</button>
+        </div>
+    )
+}
 
-// function Answer(props) {
-//     var style = {
-//         width: "100%",
-//         height: 50,
-//         color: "blue"
-//     }
-//     return (
-//         <div>
-//             <button style={style} onClick={() => props.handleClick(props.choice)}>{props.answer}</button>
-//         </div>
-//     )
-// }
+function AnswerList(props) {
+    var answers = []
+    if (!props.dataSet) {
+        return <p>Loading...</p>
+    }
+    for (let i = 0; i < props.dataSet.answers.length; i++) {
+        answers.push(<Answer choice={props.dataSet.answers[i]} handleClick={props.handleClick} answer={props.dataSet.answers[i]} />)
+    }
+    return (
+        <div>
+            {answers}
+        </div>
+    )
+}
 
-// function AnswerList(props) {
-//     var answers = []
-//     for (let i = 0; i < props.dataSet.answers.length; i++) {
-//         answers.push(<Answer choice={i} handleClick={props.handleClick} answer={props.dataSet.answers[i]} />)
-//     }
-//     return (
-//         <div>
-//             {answers}
-//         </div>
-//     )
-// }
+function QuizArea(props) {
+    var style = {
+        width: "100%",
+        display: "block",
+        textAlign: "center",
+        boxSizing: "border-box",
+        float: "left",
+        padding: "0 2em"
+    }
+    return (
+        <div style={style}>
+            <h2>Kysymys: </h2>
+            <Question dataSet={props.dataSet} />
+            <AnswerList dataSet={props.dataSet} handleClick={props.handleClick} />
+        </div>
+    )
+}
 
-// function QuizArea(props) {
-//     var style = {
-//         width: "25%",
-//         display: "block",
-//         textAlign: "center",
-//         boxSizing: "border-box",
-//         float: "left",
-//         padding: "0 2em"
-//     }
-//     return (
-//         <div style={style}>
-//             <Question dataSet={props.dataSet} />
-//             <AnswerList dataSet={props.dataSet} handleClick={props.handleClick} />
-//         </div>
-//     )
-// }
+function TotalCorrect(props) {
+    var style = {
+        display: "inline-block",
+        padding: "1em",
+        background: "#eee",
+        margin: "0 1em 0 0"
+    }
+    return (
+        <h2 style={style}>Oikeita vastauksia: {props.correct}</h2>
+    )
+}
 
-// function TotalCorrect(props) {
-//     var style = {
-//         display: "inline-block",
-//         padding: "1em",
-//         background: "#eee",
-//         margin: "0 1em 0 0"
-//     }
-//     return (
-//         <h2 style={style}>Correct: {props.correct}</h2>
-//     )
-// }
+function TotalLives(props) {
+    var style = {
+        display: "inline-block",
+        padding: "1em",
+        background: "#eee",
+        margin: "0 0 0 1em"
+    }
+    return (
+        <h2 style={style}>Elämiä jäljellä: {props.lives}</h2>
+    )
+}
 
-// function TotalLives(props) {
-//     var style = {
-//         display: "inline-block",
-//         padding: "1em",
-//         background: "#eee",
-//         margin: "0 0 0 1em"
-//     }
-//     return (
-//         <h2 style={style}>Lives: {props.lives}</h2>
-//     )
-// }
+function ScoreArea(props) {
+    var style = {
+        width: "100%",
+        display: "block",
+        textAlign: "left",
+        float: "left",
+        padding: "2em"
+    }
+    return (
+        <div style={style} >
+            <TotalCorrect correct={props.correct} />
+            <TotalLives lives={props.lives} />
+        </div>
+    )
+}
 
-// function ScoreArea(props) {
-//     var style = {
-//         width: "100%",
-//         display: "block",
-//         textAlign: "left",
-//         float: "left",
-//         padding: "2em"
-//     }
-//     return (
-//         <div style={style} >
-//             <TotalCorrect correct={props.correct} />
-//             <TotalLives lives={props.lives} />
-//         </div>
-//     )
-// }
-
-// ReactDOM.render(
-//     <Quiz />,
-//     document.getElementById("root")
-// )
+ReactDOM.render(
+    <Quiz />,
+    document.getElementById("root")
+)
