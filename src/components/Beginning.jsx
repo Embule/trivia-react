716e2c48f59554b729953dd32d14aa5372c41122
EmilyK <koskinen.emily@gmail.com>
2019-11-19 11:05:14 +0200

    created a highscore component

---
+++ src/components/Beginning.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class Beginning extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      total 
+    }
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
