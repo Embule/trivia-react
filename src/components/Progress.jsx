3cf2374b9e4aa22c3f0a6e61640bace5a8f2956b
jenniferfi <jennifer.finnila@gmail.com>
2019-11-20 11:46:14 +0200

    content in Finnish

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -4,7 +4,7 @@ export default function Progress (props) {
     return (
         <div className="progress">
         <p className="questioncounter">
-            <span>Question {props.currentQuestion+1} of 20</span>
+            <span>Kysymys {props.currentQuestion+1} / 20</span>
         </p>
             <div className="progressBar" style={{'width': ((props.currentQuestion+1) / 20) * 100 + '%'}}></div>
         </div>
